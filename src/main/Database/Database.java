d8f3e2ed34678d8176aba95552f780c40e3f13f1
julca2706 <juliacaluch@gmail.com>
2024-05-10 18:45:27 +0200

    bu

---
+++ src/main/Database/Database.java
@@ -0,0 +1,7 @@
+package Database;//Java program to set up connection and get all data from table
+
+import java.sql.*;
+
+public class Database {
+
+}
--- src/main/java/database/Database.java
+++
@@ -1,7 +0,0 @@
-package database;//Java program to set up connection and get all data from table
-
-import java.sql.*;
-
-public class Database {
-
-}
